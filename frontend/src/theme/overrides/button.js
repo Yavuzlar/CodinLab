568703ff51c99fb9a6c9a4f53c7ce0bf025cd5e1
Yunusedemirci <onlineyunusemre@gmail.com>
2024-05-06 17:24:25 +0300

    button features updated.

--- frontend/src/theme/overrides/button.js
+++ frontend/src/theme/overrides/button.js
@@ -1,16 +1,38 @@
-
-const container = theme => {
+const button = (theme) => {
   return {
     MuiButton: {
       styleOverrides: {
         root: ({ ownerState }) => ({
-          borderRadius: "1.25rem",
-          backgroundColor: ownerState.color === theme.palette.text.primary ? theme.palette.primary.main : theme.palette.common.white,
+          borderRadius: "0.938rem",
+          fontSize: "1.125rem",
+          opacity: 1,
 
+          backgroundColor: theme.palette[ownerState.color || "primary"].main,
+          color: theme.palette[ownerState.color || "primary"].contrastText,
+
+          ...(ownerState.variant == "dark" && {
+            backgroundColor: theme.palette.primary.dark,
+            color: theme.palette.text.primary,
+            "&:hover": {
+              boxShadow: `0px 3px 12px ${theme.palette.common.black}`,
+              backgroundColor: theme.palette.primary.dark,
+              color: theme.palette.text.primary,
+            },
+          }),
+
+          ...(ownerState.variant == "light" && {
+            backgroundColor: theme.palette.divider,
+            color: theme.palette.primary.dark,
+            "&:hover": {
+              boxShadow: `0px 3px 12px ${theme.palette.primary.dark}`,
+              backgroundColor: theme.palette.divider,
+              color: theme.palette.primary.dark,
+            },
+          }),
         }),
       },
-    }
-  }
-}
+    },
+  };
+};
 
-export default container
+export default button;
--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -2,15 +2,17 @@ import { Card, CardContent, Typography } from "@mui/material";
 import yavuzlarLogo from "../../../frontend/public/images/yavuzlar-logo-black.png";
 import Image from "next/image";
 import { Button } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const Home = () => {
+  const theme = useTheme();
   return (
     <div>
       <Card>
         <CardContent>
           <Typography>CodeInLab</Typography>
           <Image src={yavuzlarLogo} />
-          <Button> TEST </Button>
+          <Button variant="dark"> TEST </Button>
         </CardContent>
       </Card>
     </div>
