c08daac0782110f21bbcbe8c8b8af8e06f5a723e
Burak <brkkcygt32@gmail.com>
2024-05-07 20:40:07 +0300

    TextField overrides

--- frontend/src/theme/overrides/textfield.js
+++ frontend/src/theme/overrides/textfield.js
@@ -1,41 +1,52 @@
-
-const textfield = theme => {
+const textfield = (theme) => {
   return {
     MuiTextField: {
       styleOverrides: {
         root: ({ ownerState }) => ({
-          // "& fieldset": {
-          //     border: "1px solid #5C5C5C",
-          //     borderRadius: "0.75rem",
-          //     backgroundColor: "#FCFCFC",
-          // },
-          // // yazı rengi
-          // "& .MuiInputBase-input": {
-          //     color: "#000",
-          //     zIndex: 9,
-          // },
+          "& fieldset": {
+            border: "none",
+            borderRadius: "15px",
+            backgroundColor: "#FCFCFC",
+          },
+          // yazı rengi
+          "& .MuiInputBase-input": {
+            color: "#000",
+            zIndex: 9,
+          },
 
-          // ...(ownerState.variant === "filled" && {
-          //     borderRadius: "0.75rem",
-          //     "& input, & textarea": {
-          //         border: "unset !important",
-          //     },
-          //     "& .MuiInputBase-root": {
-          //         backgroundColor: "#FCFCFC",
-          //     },
-          //     "& .MuiInputBase-root::before": {
-          //         display: "none",
-          //         border: "unset !important",
-          //     },
-          //     "& .MuiInputBase-root::after": {
-          //         display: "none",
-          //         border: "unset !important",
-          //     },
-          // }),
+          ...(ownerState.variant === "filled" && {
+            "& input, & textarea": {
+              border: "unset !important",
+            },
+
+            "&:focus-within": {
+              backgroundColor: theme.palette.primary.light,
+              borderRadius: "15px",
+            },
+            "& .MuiInputBase-root": {
+              backgroundColor: theme.palette.primary.main,
+              borderRadius: "15px",
+            },
+            "& .MuiInputBase-root::before": {
+              display: "none",
+              border: "unset !important",
+            },
+            "& .MuiInputBase-root::after": {
+              display: "none",
+              border: "unset !important",
+            },
+          }),
+          ...(ownerState.variant === "outlined" && {
+            "& .MuiInputBase-root": {
+              "&.Mui-focused fieldset": {
+                border: "2px solid #0A3B7A",
+              },
+            },
+          }),
         }),
       },
     },
-  }
-}
+  };
+};
 
-export default textfield
+export default textfield;
