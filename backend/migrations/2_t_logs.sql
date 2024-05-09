399bdfbfbb5503f1a26112c415f2a18672dbe185
Melike Sena Çakır <melikesennaa@gmail.com>
2024-05-10 02:23:37 +0300

    Edited the database structure
    log_type and log_content tables have been merged into the logs table

--- backend/migrations/2_t_logs.sql
+++ backend/migrations/2_t_logs.sql
@@ -2,12 +2,12 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS t_logs (
     id TEXT PRIMARY KEY NOT NULL,
+    user_id TEXT NOT NULL,
+    type TEXT NOT NULL,
+    content TEXT NOT NULL,
     title TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    user_id TEXT NOT NULL,
-    log_content_id TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES t_users(id) ON DELETE CASCADE,
-    FOREIGN KEY (log_content_id) REFERENCES t_log_content(id)
+    FOREIGN KEY (user_id) REFERENCES t_users(id) ON DELETE CASCADE
 );
 -- +goose StatementEnd
 
