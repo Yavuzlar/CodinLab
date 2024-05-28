1eff8824b8003e6bc6c9b62f109c91dafcea866f
Burak <brkkcygt32@gmail.com>
2024-05-28 21:06:05 +0300

    Register form layout

--- frontend/src/views/register.js
+++ frontend/src/views/register.js
@@ -1,4 +1,4 @@
-import { Circle } from "@mui/icons-material";
+import { Circle, Google, GitHub } from "@mui/icons-material";
 import {
   Box,
   Checkbox,
@@ -10,6 +10,9 @@ import {
   Typography,
   Link,
   Button,
+  Divider,
+  Stack,
+  IconButton,
 } from "@mui/material";
 import { useState, useEffect } from "react";
 
@@ -17,15 +20,36 @@ const Register = () => {
   const [formData, setFormData] = useState(null);
   const [errors, setErrors] = useState({});
   const [formSubmit, setFormSubmit] = useState(false);
+
+  const iconSize = {
+    width: 30,
+    height: 30,
+  };
+
+  const iconBtnStyle = {
+    bgcolor: "#0A3B7A",
+    color: "#fff",
+    borderRadius: 4,
+  };
+
+  const inputLabelStyle = {
+    sx: {
+      color: "#0A3B7A",
+      font: "normal normal bold 18px/23px Outfit",
+      ml: 1,
+    },
+  };
+
   return (
-    <Container>
+    <Container sx={{ display: "flex", justifyContent: "center" }}>
       <Box
         sx={{
           bgcolor: "#3894D0",
           borderRadius: "16px",
-          maxWidth: "812px",
+          width: "812px",
+          height: "800px",
         }}>
-        <Grid container justifyContent="center" direction="column" px={10}>
+        <Grid container direction="column" px={10}>
           <Box
             sx={{
               display: "flex",
@@ -34,26 +58,46 @@ const Register = () => {
               gap: 1,
               my: 5,
             }}>
-            <Circle sx={{ width: 40, height: 40 }} />
-            <Typography textAlign="center" variant="h4">
+            <Circle sx={{ width: 40, height: 40, mr: 1 }} />
+            <Typography
+              textAlign="center"
+              variant="body1"
+              fontFamily="Outfit"
+              fontWeight="600"
+              fontSize="35px">
               CodinLab
             </Typography>
           </Box>
           <FormControl>
-            <Grid
-              container
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              gap={2}>
-              <TextField name="fullname" label="Fullname" fullWidth />
-              <TextField name="username" label="Username" fullWidth />
-              <TextField name="email" label="E-mail" fullWidth />
-              <TextField name="password" label="Password" fullWidth />
+            <Grid container direction="column" gap={2}>
+              <TextField
+                name="fullname"
+                label="Fullname"
+                InputLabelProps={inputLabelStyle}
+                fullWidth
+              />
+              <TextField
+                name="username"
+                label="Username"
+                InputLabelProps={inputLabelStyle}
+                fullWidth
+              />
+              <TextField
+                name="email"
+                label="E-mail"
+                InputLabelProps={inputLabelStyle}
+                fullWidth
+              />
+              <TextField
+                name="password"
+                label="Password"
+                InputLabelProps={inputLabelStyle}
+                fullWidth
+              />
               <FormControlLabel
-                control={<Checkbox />}
+                control={<Checkbox sx={{ color: "#fff" }} />}
                 label={
-                  <Typography>
+                  <Typography fontSize={18} fontFamily={"Outfit"}>
                     I accept the{" "}
                     <Link color={"#fffddd"} href="#">
                       privacy policy & terms
@@ -61,9 +105,41 @@ const Register = () => {
                   </Typography>
                 }
               />
-              <Button>Sign Up</Button>
+              <Button
+                sx={{
+                  bgcolor: "#0A3B7A",
+                  font: "normal normal 18px/23px Outfit",
+                  fontWeight: "600",
+                  textTransform: "capitalize",
+                  py: 2,
+                }}
+                fullWidth>
+                Sign Up
+              </Button>
             </Grid>
           </FormControl>
+          <Divider sx={{ mt: 2 }}>or</Divider>
+          <Stack direction="row" justifyContent="center" gap={3} mt={3}>
+            <IconButton variant="contained" sx={iconBtnStyle}>
+              <Google sx={iconSize} />
+            </IconButton>
+            <IconButton variant="contained" sx={iconBtnStyle}>
+              <GitHub sx={iconSize} />
+            </IconButton>
+          </Stack>
+          <Typography
+            variant="body1"
+            textAlign={"center"}
+            mt={4}
+            fontFamily={"Outfit"}>
+            Already have an account?{" "}
+            <Link
+              href="#"
+              color="#0A3B7A"
+              sx={{ fontWeight: "600", textDecoration: "none", ml: 1 }}>
+              Login
+            </Link>
+          </Typography>
         </Grid>
       </Box>
     </Container>
