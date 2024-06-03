ed75ef96ec2a55efcfc0df1ed14cda316600188c
Burak <brkkcygt32@gmail.com>
2024-06-03 14:37:21 +0300

    common.js updates for register page

--- frontend/src/theme/breakpoints/index.js
+++ frontend/src/theme/breakpoints/index.js
@@ -1,15 +1,16 @@
 const breakpoints = {
-    values: {
-        xs: 0,
-        sm: 600,
-        smd: 768,
-        md: 900,
-        mdlg: 1024,
-        lg: 1200,
-        lgPlus: 1280,
-        lgXl: 1440,
-        xl: 1536,
-    },
-}
+  values: {
+    xs: 0,
+    sm: 600,
+    smd: 768,
+    md: 900,
+    mdlg: 1024,
+    lg: 1200,
+    lgPlus: 1280,
+    lgXl: 1440,
+    xl: 1536,
+    xxl: 1920,
+  },
+};
 
-export default breakpoints
+export default breakpoints;
