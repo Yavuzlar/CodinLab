b3830f1047050ef7be8cfe52206363bb87d76814
Burak <brkkcygt32@gmail.com>
2024-08-22 18:14:26 +0300

    road details page completed with a few flaws

---
+++ frontend/src/pages/roads/[language]/index.js
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router";
+import RoadDetails from "src/views/road-detail";
+
+const RoadDetailPage = () =>  {
+
+    const router = useRouter();
+    const language = router.query.language;
+    
+    return <RoadDetails language={language} />
+}
+
+RoadDetailPage.acl = {
+  action: "read",
+  permission: "roads",
+};
+
+export default RoadDetailPage;
