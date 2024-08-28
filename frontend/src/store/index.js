0d2ec1875cf49e62ae8597982a117e6e56427b4f
yunusedemirci <onlineyunusemre@gmail.com>
2024-08-29 02:22:10 +0300

    For Rebase main

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,11 +3,14 @@ import { configureStore } from '@reduxjs/toolkit'
 // ** Reducers
 import authSlice from './auth/index.js'
 import userSlice from './user/userSlice'
+import statisticsSlice from './statistics/statisticsSlice.js'
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
-    user : userSlice // Include the user reducer
+    user : userSlice, // Include the user reducer
+    statistics: statisticsSlice, 
+
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
---
+++ frontend/src/store/statistics/statisticsSlice.js
@@ -0,0 +1,48 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  loading: false,
+  data: [],
+  error: false,
+};
+
+export const fetchAdvancement = createAsyncThunk(
+  "statistics/fetchAdvancement",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios({
+        url: "/api/v1/private/home/advancement",
+        method: "GET",
+      });
+      if (response.status === 200) {
+        return response.data.data; 
+      }
+    } catch (error) {
+      return rejectWithValue(response.message); 
+    }
+  }
+);
+
+const statisticsSlice = createSlice({
+  name: "statistics",
+  initialState: initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAdvancement.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchAdvancement.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchAdvancement.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default statisticsSlice.reducer;
