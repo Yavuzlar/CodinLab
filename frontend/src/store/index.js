61baa87b903e87ea77dfec3cda801add83913e86
Burak Bıçakcıoğlu <burakbicakci14@gmail.com>
2024-08-30 00:48:28 +0300

    add inventories for home page

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit'
 // ** Reducers
 import authSlice from './auth/index.js'
 import userSlice from './user/userSlice'
+import languageSlice from './language/languageSlice' // Import the language slice
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
-    user : userSlice // Include the user reducer
+    user : userSlice,
+    language : languageSlice // Include the language reducer
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
