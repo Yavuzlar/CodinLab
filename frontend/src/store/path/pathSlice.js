baa36e64010d89dccc761684aac84e8267cef843
burkocyigit <brkkcygt32@gmail.com>
2024-09-20 16:32:05 +0300

    get code template, reset path button

--- frontend/src/store/path/pathSlice.js
+++ frontend/src/store/path/pathSlice.js
@@ -29,23 +29,6 @@ export const fetchPathById = createAsyncThunk(
   }
 );
 
-export const resetPathById = createAsyncThunk(
-  "path/resetPathById",
-  async (data, { rejectWithValue }) => {
-    try {
-      const response = await axios({
-        method: "GET",
-        url: `/api/v1/private/road/reset/${data.programmingid}/${data.pathId}`,
-      });
-      if (response.status === 200) {
-        return response.data;
-      }
-    } catch (error) {
-      return rejectWithValue(response.message || error.message);
-    }
-  }
-);
-
 const pathSlice = createSlice({
     name: "path",
     initialState: initialState,
@@ -63,18 +46,6 @@ const pathSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         })
-        .addCase(resetPathById.pending, (state) => {
-            state.loading = true;
-            state.error = false;
-        })
-        .addCase(resetPathById.fulfilled, (state, action) => {
-            state.data = action.payload;
-            state.loading = false;
-        })
-        .addCase(resetPathById.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
-        });
     }
 })
 
