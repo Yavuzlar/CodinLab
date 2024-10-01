003ab408788d6aa5a393a9dad0fd247ebe30de27
Burak Bıçakcıoğlu <burakbicakci14@gmail.com>
2024-10-01 14:49:53 +0300

    Integration of Get All Labs

--- frontend/src/components/cards/LanguageProgress.js
+++ frontend/src/components/cards/LanguageProgress.js
@@ -5,15 +5,16 @@ import {
   useMediaQuery,
   Grid,
   Box,
-  Button
+  Button,
 } from "@mui/material";
 import Image from "next/image";
 import LinearProgess from "../progress/LinearProgess";
 import Translations from "../Translations";
 import labIcon from "src/assets/icons/icons8-test-tube-100.png";
 import roadIcon from "src/assets/icons/icons8-path-100.png";
+import { useRouter } from "next/router";
 
-const LanguageProgress = ({ language,type }) => {
+const LanguageProgress = ({ language, type }) => {
   const _lg = useMediaQuery((theme) => theme.breakpoints.down("lg"));
   const _md = useMediaQuery((theme) => theme.breakpoints.down("md"));
   const _sm = useMediaQuery((theme) => theme.breakpoints.down("sm"));
@@ -30,9 +31,13 @@ const LanguageProgress = ({ language,type }) => {
   // }
   // console.log("language", language.totalLabs);
 
-    // Determine icon and data based on the type
-    const icon = type === "road" ? roadIcon : labIcon;
-    const data = type === "road" ? language.totalRoads : language.totalLabs;
+  // Determine icon and data based on the type
+  const icon = type === "road" ? roadIcon : labIcon;
+  const data = type === "road" ? language.totalRoads : language.totalLabs;
+
+  const router = useRouter();
+
+  console.log("language", language);
 
   return (
     <Card>
@@ -46,90 +51,148 @@ const LanguageProgress = ({ language,type }) => {
         }}
       >
         <img
-          src={"api/v1/"+language.iconPath}
+          src={"api/v1/" + language.iconPath}
           alt={language.name}
           width={56}
           height={56}
         />
 
-        {
-          language.percentage == 0
-            ? _lg && !_md
-              ? <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.25rem' }}>
-                  <Typography
-                    variant="h5"
-                    sx={{
-                      width: '100%',
-                      fontWeight: 600,
-                      textOverflow: 'ellipsis',
-                      overflow: 'hidden'
-                    }}
-                  >
-                    {language.name}
-                  </Typography>
-
-                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.5rem', minWidth: 'fit-content' }}>
-                    <Image src={icon} alt={"icon"} width={20} height={20} />
-
-                    <Typography variant="infoText2">{data}</Typography>
-                  </Box>
-      
-                </Box>
-
-                <Button variant="dark" sx={{ textTransform: 'none', minWidth: '80px' }}>
-                  <Typography variant="infoText"><Translations text="start" /></Typography>
-                </Button>
-              </Box>
-              : <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+        {language.percentage == 0 ? (
+          _lg && !_md ? (
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "0.25rem",
+                }}
+              >
                 <Typography
                   variant="h5"
                   sx={{
-                    maxWidth: 'calc(100% - 200px)',
-                    width: '100%',
+                    width: "100%",
                     fontWeight: 600,
-                    textOverflow: 'ellipsis',
-                    overflow: 'hidden'
+                    textOverflow: "ellipsis",
+                    overflow: "hidden",
                   }}
                 >
                   {language.name}
                 </Typography>
 
-                <Box sx={{ display: 'flex', gap: '1rem', width: '100%', justifyContent: 'end' }}>
-                  <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.5rem', minWidth: 'fit-content' }}>
-                    <Image src={icon} alt={"icon"} width={24} height={24} />
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "0.5rem",
+                    minWidth: "fit-content",
+                  }}
+                >
+                  <Image src={icon} alt={"icon"} width={20} height={20} />
 
-                    <Typography variant="infoText">{data}</Typography>
-                  </Box>
-
-                  <Button variant="dark" sx={{ textTransform: 'none', minWidth: '80px' }}>
-                    <Typography variant="infoText"><Translations text="start" /></Typography>
-                  </Button>
+                  <Typography variant="infoText2">{data}</Typography>
                 </Box>
               </Box>
-            : <Grid container spacing={0}>
-              <Grid item xs={12}>
-                <Typography
-                  variant="h5"
-                  sx={{ fontWeight: 600 }}
-                >
-                  {language.name}
+
+              <Button
+                variant="dark"
+                sx={{ textTransform: "none", minWidth: "80px" }}
+              >
+                <Typography variant="infoText">
+                  <Translations text="start" />
                 </Typography>
-              </Grid>
+              </Button>
+            </Box>
+          ) : (
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
+              <Typography
+                variant="h5"
+                sx={{
+                  maxWidth: "calc(100% - 200px)",
+                  width: "100%",
+                  fontWeight: 600,
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                }}
+              >
+                {language.name}
+              </Typography>
 
-              <Grid item xs={12}>
-                <LinearProgess progress={language.percentage} />
-              </Grid>
-
-              <Grid item xs={12}>
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  gap: "1rem",
+                  width: "100%",
+                  justifyContent: "end",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "0.5rem",
+                    minWidth: "fit-content",
+                  }}
+                >
                   <Image src={icon} alt={"icon"} width={24} height={24} />
 
                   <Typography variant="infoText">{data}</Typography>
                 </Box>
-              </Grid>
+
+                <Button
+                  variant="dark"
+                  sx={{ textTransform: "none", minWidth: "80px" }}
+                  onClick={() => {
+                    if (type === " ") {
+                      router.push(`/road/${language.id}`);
+                    } else if (type === "lab") {
+                      router.push(`/labs/${language.id}`);
+                    }
+                  }}
+                >
+                  <Typography variant="infoText">
+                    <Translations text="start" />
+                  </Typography>
+                </Button>
+              </Box>
+            </Box>
+          )
+        ) : (
+          <Grid container spacing={0}>
+            <Grid item xs={12}>
+              <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                {language.name}
+              </Typography>
             </Grid>
-        }
+
+            <Grid item xs={12}>
+              <LinearProgess progress={language.percentage} />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Box
+                sx={{ display: "flex", alignItems: "center", gap: "0.5rem" }}
+              >
+                <Image src={icon} alt={"icon"} width={24} height={24} />
+
+                <Typography variant="infoText">{data}</Typography>
+              </Box>
+            </Grid>
+          </Grid>
+        )}
       </CardContent>
     </Card>
   );
