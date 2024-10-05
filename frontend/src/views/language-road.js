39ad9868c51b348e0517319cdcf4f84e7c4d3e39
yunusedemirci <onlineyunusemre@gmail.com>
2024-10-05 17:58:49 +0300

    for rebase main

--- frontend/src/views/language-road.js
+++ frontend/src/views/language-road.js
@@ -42,7 +42,7 @@ const LanguageRoad = ({ language = "", pathId }) => {
   const [template, setTemplate] = useState("");
 
   useEffect(() => {
-    setProgrammingId(getProgrammingId[language]);
+    setProgrammingId(language);
   }, [language]);
 
   useEffect(() => {
