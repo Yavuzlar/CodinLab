6f2aa1956684c315ef476683fdaba7e2f8176611
Melike Sena Çakır <melikesennaa@gmail.com>
2025-01-21 01:27:30 +0300

    cpp and go roads revised

--- backend/object/paths/golang/main.sh
+++ backend/object/paths/golang/main.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-test=(-tests-) # test dizisi tanımlandı
+test=(-tests-) 
 
-export TERM=xterm  # TERM değişkeni ayarlandı
+export TERM=xterm  
 
-# Eğer test dizisi boşsa, bir kere çalıştır. Cevap gerekmeyen öğrenmek için olan bir pathdir.
+
+# If the test array is empty, this will run. It means that it is a path for learning, not for getting an answer.
 if [ ${#test[@]} -eq 0 ]; then
     result=$(node main.js)
     echo "Test Passed|||$result|||_|||_"
     exit 0
 fi
 
-# Test döngüsü
+# Test loop
 for i in "${!test[@]}"; do
     expected_result="${test[$i]}"
 
-    # GO dosyasını çalıştır 
     compile_output=$(go build -o main ../main.go 2>&1)
 
         if [ $? -ne 0 ]; then 
@@ -25,7 +25,7 @@ for i in "${!test[@]}"; do
 
     result=$(go run ../main.go)  
 
-    # Sonucu beklenen sonuç ile karşılaştır
+    # the result will be compared with the expected result
     if [[ "$result" == "$expected_result" ]]; then
         echo "Test Passed|||$result|||_|||_"
     else
